c1d2b7bd5afd2f294752b4fd43f4354e6d0b7919
Shrek75 <freshine0@gmail.com>
2026-03-04 16:56:47 +0900

    타일맵, 오버랩박스 점프

--- My personal Project/Assets/Scripts/PlayerMove.cs
+++ My personal Project/Assets/Scripts/PlayerMove.cs
@@ -1,24 +1,80 @@
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    PlayerInput playerInput;
+    BoxCollider2D coll;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    Vector2 moveInput;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    void OnEnable()
+    {
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+
+        moveAction.performed += OnMove;
+        moveAction.canceled += OnMove;
+        jumpAction.performed += OnJump;
+    }
+
+    void OnDisable()
+    {
+        moveAction.performed -= OnMove;
+        moveAction.canceled -= OnMove;
+        jumpAction.performed -= OnJump;
+    }
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (anim.GetBool("isJump")) return;
+        rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    
+    void Update()
+    {
+        if (moveInput.x > 0) spriteRenderer.flipX = false;
+        else if(moveInput.x < 0) spriteRenderer.flipX = true;
+        if (rigid.linearVelocityX == 0) anim.SetBool("isWalking", false);
+        else anim.SetBool("isWalking", true);
+
     }
 
     void FixedUpdate()
     {
-        float h = 0;
+        rigid.linearVelocity = new Vector2(moveInput.x * moveSpeed, rigid.linearVelocity.y);
 
-        if (Keyboard.current.aKey.isPressed)
-            h = -1;
-        else if (Keyboard.current.dKey.isPressed)
-            h = 1;
-
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        Debug.DrawRay(rigid.position, Vector2.down);
+        var c = Physics2D.OverlapBox(rigid.position + Vector2.down * (coll.size.y / 2f), new Vector2(coll.size.x * 0.9f, 0.01f), 0, LayerMask.GetMask("Platform"));
+        if (c != null)
+        {
+            //Debug.Log(c.name);
+            anim.SetBool("isJump", false);
+        }
+        else anim.SetBool("isJump", true);
     }
 }
